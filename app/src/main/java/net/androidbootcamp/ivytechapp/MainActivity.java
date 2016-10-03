f86092687f72e728938de3cec431ff2ebb306992
funkateeer <funkateeer@gmail.com>
2016-10-03 11:58:25 -0400

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/ivytechapp/MainActivity.java
@@ -0,0 +1,123 @@
+package net.androidbootcamp.ivytechapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import net.androidbootcamp.ivytechapp.CustomListAdapter.CustomListAdapter;
+
+
+//public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
+
+    ListView list;
+    String[] itemname = {
+            "Campus Maps",
+            "Instructor Info",
+            "Calendar of Events",
+            "Dining"
+    };
+
+    Integer[] imgid = {
+            R.drawable.googlemapsicon,
+            R.drawable.ic_launcher_contactinfo,
+            R.drawable.calendaricon,
+            R.drawable.restauranticon,
+    };
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomListAdapter adapter = new CustomListAdapter(this, itemname, imgid);
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem = itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        switch(position){
+            case 0:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ivytech.edu/northeast/calendar/")));
+                break;
+            case 1:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ivytech.edu/northeast/calendar/")));
+                break;
+
+            case 2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ivytech.edu/northeast/calendar/")));
+                break;
+
+            case 3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ivytech.edu/northeast/calendar/")));
+                break;
+        }
+    }
+}
