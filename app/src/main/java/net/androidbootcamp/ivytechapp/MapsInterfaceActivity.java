e752e8a996620d52d6197678bb25afea025927a1
funkateeer <funkateeer@gmail.com>
2016-11-09 10:00:10 -0500

    minor tweaks

--- app/src/main/java/net/androidbootcamp/ivytechapp/MapsInterfaceActivity.java
+++ app/src/main/java/net/androidbootcamp/ivytechapp/MapsInterfaceActivity.java
@@ -1,8 +1,11 @@
 package net.androidbootcamp.ivytechapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MapsInterfaceActivity extends AppCompatActivity {
@@ -14,5 +17,12 @@ public class MapsInterfaceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maps_interface);
         editText = (EditText)findViewById(R.id.editText);
         editText.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MapsInterfaceActivity.this, MapsActivity.class));
+            }
+        });
     }
 }
